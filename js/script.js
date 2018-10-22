3f8049df584fc417beae00e55b4d19c394e6d2ac
AVINASH <avinash29july@gmail.com>
2018-10-23 03:38:22 +0530

    updated with clone copy

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 let colors = ['#2E3F4F', '#489C85', '#A2B55B', '#E99E32', '#B54A37'];
 document.getElementsByClassName("page-reload")[0].style.display ="none";
+document.getElementsByClassName("show-after-generate")[0].style.display = "none";
 let canvas = [];
 let generateCanvas = () => {
     const numberOfCanvas = ["3", "4", "5"];
@@ -11,20 +12,16 @@ let generateCanvas = () => {
             backgroundColor: "#ffffff",
             width: 500,
             height: 160,
-            targetFindTolerance: 2,
-            selection: false,
-            hoverCursor: 'pointer'
+            draggable: true
         }));
-        console.log("1",canvas);
     }
     document.getElementsByClassName("generate")[0].style.display = "none";
     document.getElementsByClassName("page-reload")[0].style.display = "block";
+    document.getElementsByClassName("show-after-generate")[0].style.display = "block";
 };
-
 const appendCanvasElement = (i) => {
     let heightElement = document.createElement("div");
     heightElement.setAttribute("class","height-20");
-
     /**
      * adding canvas randomly while creating from generate button
      * @type {Element}
@@ -34,8 +31,6 @@ const appendCanvasElement = (i) => {
     createElement.setAttribute("id","id_"+i);
     getElement.appendChild(createElement);
     getElement.appendChild(heightElement);
-
-
     /**
      * select drop down for canvas id
      * @type {Element}
@@ -46,86 +41,115 @@ const appendCanvasElement = (i) => {
     createSelectElement.value = "id_" + i;
     selectElement.appendChild(createSelectElement);
 };
-
-
 const generateShape = () => {
     let selectedId = document.getElementsByClassName("inputGroupSelect")[0].value;
     let id = selectedId.split("_")[1];
-
     let selectedShape = document.getElementsByClassName("inputShapeSelect")[0].value;
-
     let generateShape = checkShape(selectedShape);
-    //canvas[id].add(group);
-    //canvas[id].add(circle2);
-    canvas[id].add(generateShape);
-
+    let isActiveObject = canvas[id].getActiveObject();
+    if(isActiveObject) {
+        if(isActiveObject.type === selectedShape) {
+            let clipboard;
+            canvas[id].getActiveObject().clone(function(cloned) {
+                clipboard = cloned;
+            });
+            clipboard.clone(function(clonedObj) {
+                canvas[id].discardActiveObject();
+                clonedObj.set({
+                    left: clonedObj.left + 10,
+                    top: clonedObj.top + 10,
+                    evented: true,
+                });
+                if (clonedObj.type === 'activeSelection') {
+                    // active selection needs a reference to the canvas.
+                    clonedObj.canvas[id] = canvas[id];
+                    clonedObj.forEachObject(function(obj) {
+                        canvas[id].add(obj);
+                        canvas[id].setActiveObject(obj);
+                    });
+                    // this should solve the unselectability
+                    clonedObj.setCoords();
+                } else {
+                    canvas[id].add(clonedObj);
+                    canvas[id].setActiveObject(clonedObj);
+                }
+            })
+        }
+        else  {
+            canvas[id].add(generateShape);
+            canvas[id].setActiveObject(generateShape);
+        }
+    }
+     else {
+        canvas[id].add(generateShape);
+        canvas[id].setActiveObject(generateShape);
+    }
+    canvas[id].requestRenderAll();
 };
 
 const checkShape =(shapeValue) => {
     switch (shapeValue) {
         case "rect": {
             return rect;
-            break;
         }
         case "circle": {
             return circle;
-            break;
         }
         case "triangle": {
             return triangle;
-            break;
         }
         case "group": {
             return group;
-            break
         }
         default: {
             console.log("no shape selected");
-            break;
         }
     }
 };
 
+
 let rect = new fabric.Rect({
-    left: 20,
-    top: 20,
+    left: 100,
+	top: 50,
     fill: _.sample(colors),
     width: 40,
-    height: 50
+    height: 50,
 });
 
 let triangle = new fabric.Triangle({
     width: 40,
     height: 50,
     fill: _.sample(colors),
-    left: 70,
-    top: 50
+    left: 100,
+	top: 50,
 });
 
 let circle = new fabric.Circle({
     radius: 25,
     fill: _.sample(colors),
-    left: 130,
-    top: 50
+    left: 100,
+	top: 50,
 });
 
 
-let circle1 = new fabric.Circle({
-    radius: 20,
+let circleTwo = new fabric.Circle({
+    radius: 35,
     fill: _.sample(colors),
     left: 0
 });
-let circle2 = new fabric.Circle({
-    radius: 30,
+let triangleTwo = new fabric.Triangle({
+    width: 70,
+    height: 70,
     fill: _.sample(colors),
-    left: 100
+    left: 140,
 });
-let circle3 = new fabric.Circle({
-    radius: 10,
+let rectangleTwo = new fabric.Rect({
+    left: 70,
     fill: _.sample(colors),
-    left: 200
+    width: 70,
+    height: 70
 });
-let group = new fabric.Group([ circle1, circle2, circle3 ], {
-    left: 0,
-    top: 20
+let group = new fabric.Group([circleTwo, rectangleTwo, triangleTwo], {
+    left: 200,
+    top: 50
 });
